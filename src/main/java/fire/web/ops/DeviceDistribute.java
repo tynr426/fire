092ecc479d20f6ef18af4128e1629c6d8f860629
pc <pc@F0X0W2J6PRGKFTO>
2017-10-14 17:04:48 +0800

    设备列表

--- src/main/java/fire/web/ops/DeviceDistribute.java
+++ src/main/java/fire/web/ops/DeviceDistribute.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import fire.common.entity.DeviceResult;
+import fire.sdk.utils.ConvertUtils;
 import fire.sdk.utils.DTOBeanUtils;
 import fire.web.service.DeviceService;
 import fire.web.utils.JsonResult;
@@ -29,6 +30,10 @@ public class DeviceDistribute extends Distribute {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		if(sp.Action.equals("addDevice")){
 			addDevice(req,resp);
+		}else if(sp.Action.equals("getDevice")){
+			getDevice(req,resp);
+		}else if(sp.Action.equals("showDevice")){
+			showDevice(req,resp);
 		}
 	}
 	private void addDevice(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
@@ -45,6 +50,20 @@ public class DeviceDistribute extends Distribute {
 		String str=Utils.objectToJson(new JsonResult(deviceService.addDevice(device)));
 		resp.getWriter().write(str);
 	}
-
+	private void getDevice(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		int id=ConvertUtils.toInt(req.getParameter("Id"));
+		resp.setContentType("text/javascript; charset=utf-8"); 
+		String str=Utils.objectToJson(new JsonResult(deviceService.getDevice(id)));
+		resp.getWriter().write(str);
+	}
+	private void showDevice(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		int companyId=ConvertUtils.toInt(req.getParameter("CompanyId"));
+		int index=ConvertUtils.toInt(req.getParameter("Index"));
+		int size=ConvertUtils.toInt(req.getParameter("Size"));
+		resp.setContentType("text/javascript; charset=utf-8"); 
+		String str=Utils.objectToJson(new JsonResult(deviceService.getDevicePage(companyId, index, size)));
+		resp.getWriter().write(str);
+	}
+	
 
 }
